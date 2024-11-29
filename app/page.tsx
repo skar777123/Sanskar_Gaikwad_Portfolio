d9ea6ef904fe5ba6470dec871c667a45165fcffb
skar777123 <sanskargaikwad206@gmail.com>
2024-11-29 10:45:43 +0530

    "Updated dependencies and added NeonCursor component to Home page"

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,13 @@ import Footer from "@/components/Footer";
 import { ThreeDCardDemo } from "@/components/Cards";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Skills from "@/components/Skills";
+import NeonCursor from "@/components/ui/useCanvasCursor";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <NeonCursor />
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
